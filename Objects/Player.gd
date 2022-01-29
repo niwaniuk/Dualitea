f2df22af5d2678265de0fa3bbfa27d8c71c35d00
Natalie Iwaniuk <niwaniuk@ualberta.ca>
2022-01-29 16:41:44 -0700

    Adds player, button script, basic debug tiles.

---
+++ Objects/Player.gd
@@ -0,0 +1,37 @@
+extends KinematicBody2D
+
+class_name Player
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export (int) var speed = 500
+export (int) var jump_speed = -900
+export (int) var gravity = 2500
+
+export (float, 0, 1.0) var friction = 0.25
+export (float, 0, 1.0) var acceleration = 0.25
+
+var velocity = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var dir = 0
+	if Input.is_action_pressed("walk_right"):
+		dir += 1
+	if Input.is_action_pressed("walk_left"):
+		dir -= 1
+	if dir != 0:
+		velocity.x = lerp(velocity.x, dir * speed, acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0, friction)
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = jump_speed
